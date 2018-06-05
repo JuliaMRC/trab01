ee0472768b88a9a9e0a49c67a8ee4724308a40a6
Ewerson Vieira <ewersonv@gmail.com>
2018-06-05 15:43:18 -0300

    Create drop.sql

---
+++ sql/drop.sql
@@ -0,0 +1,11 @@
+drop table historico;
+drop table cartao;
+drop table usuario;
+drop table logradouro;
+drop table bairro;
+drop table cidade;
+drop table horario;
+drop table itinerario;
+drop table tipo_horario;
+drop table via;
+drop table linha;
