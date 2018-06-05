bbcd01634cca264bcfdb6a2ecb9e89ab7efeb07e
Julio Cesar <37442681+Juliocsr94@users.noreply.github.com>
2018-06-05 16:30:57 -0300

    Envio arquivo select.sql

---
+++ sql/select.sql
@@ -0,0 +1,21 @@
+SELECT * FROM linha;
+
+SELECT * FROM cidade;
+
+SELECT * FROM bairro;
+
+SELECT * FROM logradouro;
+
+SELECT * FROM usuario;
+
+SELECT * FROM cartao;
+
+SELECT * FROM via;
+
+SELECT * FROM itinerario;
+
+SELECT * FROM tipo_horario;
+
+SELECT * FROM horario;
+
+SELECT * FROM historico;
