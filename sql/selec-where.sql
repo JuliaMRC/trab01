b9bea8c226483f0ea2d5d7ab79f7c7d7b14d500f
Julio Cesar <37442681+Juliocsr94@users.noreply.github.com>
2018-06-06 16:55:56 -0300

    Envio do arquivo select-where

---
+++ sql/selec-where.sql
@@ -0,0 +1,14 @@
+SELECT * FROM historico
+WHERE historico_valor = '3,40';
+
+SELECT * FROM itinerario
+WHERE via_id = '8';
+
+SELECT * FROM itinerario
+WHERE numero_linha = '507';
+
+SELECT * FROM logradouro
+WHERE desc_tipo = 'Avenida';
+
+SELECT * FROM cartao
+WHERE cartao_titular = 'Joao da Silva';
