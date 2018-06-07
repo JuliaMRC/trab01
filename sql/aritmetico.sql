0382f241d87604156fd08d554fbc4051c91e5c24
Julio Cesar <37442681+Juliocsr94@users.noreply.github.com>
2018-06-07 17:43:29 -0300

    envio do arquivo aritmetico.sql

---
+++ sql/aritmetico.sql
@@ -0,0 +1,10 @@
+SELECT SUM(historico_valor)
+FROM historico
+WHERE historico_valor = '3,40';
+
+SELECT SUM(historico_valor)
+FROM historico
+WHERE historico_valor = '1,70';
+
+SELECT SUM(historico_valor)
+FROM historico;
