2a2a945b86218c1b5805b220de74712364da2704
Ewerson Vieira <ewersonv@gmail.com>
2018-07-02 22:42:15 -0300

    Update 9_7.sql

--- sql/9_7.sql
+++ sql/9_7.sql
@@ -3,4 +3,4 @@ select numero_linha, count(sentido) from itinerario group by numero_linha;
 select historico_linha, count(historico_id), sum(historico_valor) from historico group by historico_linha order by historico_linha;
 select count(id_cidade), flag_estado from cidade group by flag_estado;
 select count(cep), desc_tipo from logradouro group by desc_tipo;
-select count(sentido), via_id, numero_linha from itinerario group by numero_linha, via_id order by numero_linha, via_id;
+select count(sentido), via_id, numero_linha from itinerario group by numero_linha, via_id order by numero_linha, via_id limit 10;
